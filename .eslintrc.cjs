b6443165162e64d1643a6c53e70043f3f1ca8195
Marcus Cruz <marcuseduardoc@gmail.com>
2024-07-08 11:28:52 -0300

    fix: eslint resolve

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,10 @@ const config = {
   "rules": {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
